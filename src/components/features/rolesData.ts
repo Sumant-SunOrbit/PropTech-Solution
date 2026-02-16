544ae61835a0225d640cf60260e806357750ff86
Sakshi Jadhav <sakshi.jadhav@sunorbit.in>
2026-02-16 11:27:53 +0530

    created feature solution and working page

---
+++ src/components/features/rolesData.ts
@@ -0,0 +1,49 @@
+import { FaUserGear } from "react-icons/fa6";
+import { GoCrossReference } from "react-icons/go";
+import {
+  HiOutlineUserGroup,
+  HiOutlineHome,
+} from "react-icons/hi";
+
+export const rolesData = [
+  {
+    title: "Admin Control",
+    icon: FaUserGear,
+    points: [
+      "Multi-society overview",
+      "SLA & escalation rules",
+      "Staff performance tracking",
+      "Real-time analytics",
+    ],
+  },
+  {
+    title: "Staff Operations",
+    icon: HiOutlineUserGroup,
+    points: [
+      "Smart ticket assignment",
+      "Priority-based tasks",
+      "Easy status updates",
+      "Mobile-friendly access",
+    ],
+  },
+  {
+    title: "Resident Experience",
+    icon: HiOutlineHome,
+    points: [
+      "Raise issues in seconds",
+      "Upload photos & videos",
+      "Live status tracking",
+      "Faster resolutions",
+    ],
+  },
+  {
+    title: "Refer and Earn",
+    icon: GoCrossReference,
+    points: [
+      "Raise issues in seconds",
+      "Upload photos & videos",
+      "Live status tracking",
+      "Faster resolutions",
+    ],
+  }
+];
