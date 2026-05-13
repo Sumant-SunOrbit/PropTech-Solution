2ef916738b9352f387b1f71f2ba3a901e04d753d
rohanmaindarkar <rohan.maindarkar@sunorbit.in>
2026-05-13 11:25:54 +0530

    Added new setting

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: "export",
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.jsdelivr.net",
-      },
-    ],
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "cdn.jsdelivr.net",
+    //   },
+    // ],
+    unoptimized:true,
   },
 };
 
